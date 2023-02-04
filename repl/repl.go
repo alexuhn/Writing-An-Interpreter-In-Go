c4c08c5852ff6fff4ad074908b77d1576b04a0c4
ksh <alexuhyun@gmail.com>
2023-02-04 15:17:35 +0900

    Implement initial REPL and main fucntion

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+// REPL(Read Eval Print Loop)
+// 입력을 읽고, 인터프리터에 보내 평가하고, 인터프리터의 결과물을 출력하는 동작을 반복
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprint(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
