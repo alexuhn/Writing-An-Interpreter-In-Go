ca637f1cfc9d0040f1b5f9542bcce410b430752a
ksh <alexuhyun@gmail.com>
2023-03-04 12:15:58 +0900

    Implement Integer, Boolean evaluators

---
+++ evaluator/evaluator.go
@@ -0,0 +1,47 @@
+package evaluator
+
+import (
+	"monkey/ast"
+	"monkey/object"
+)
+
+var (
+	// Null과 Boolean 경우 인스턴스를 매번 새로 만들 필요가 없으므로
+	// 미리 만들어둔 아래 인스턴스를 참조(reference)하도록 한다.
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	// 명령문
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// 표현식
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
